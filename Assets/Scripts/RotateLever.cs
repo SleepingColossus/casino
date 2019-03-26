d8ac12669e96ebf24a2b073098e859664d5b55bf
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-03-26 23:31:58 +0000

    Add rotation scripts

---
+++ Assets/Scripts/RotateLever.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLever : MonoBehaviour
+{
+    private bool _shouldRotate;
+    private const float _rotateBy = 90;
+    private int _rotationDirection;
+    private float _remainingRotation;
+    private float _rotationSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _shouldRotate = true;
+        _rotationDirection = -1;
+        _rotationSpeed = 1;
+        _remainingRotation = _rotateBy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_shouldRotate)
+        {
+            float yAngle = _rotationSpeed * _rotationDirection;
+            transform.Rotate(0, yAngle, 0);
+
+            _remainingRotation -= _rotationSpeed;
+
+            if (_remainingRotation <= 0)
+            {
+                _shouldRotate = false;
+                _rotationDirection *= -1;
+                _remainingRotation = _rotateBy;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpinReel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class SpinReel : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+
+    private bool _shouldRotate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _shouldRotate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_shouldRotate)
+        {
+            transform.Rotate(0, 0, -rotationSpeed);
+        }
+    }
+
+    public void StartRotation()
+    {
+        _shouldRotate = true;
+    }
+
+    public void StopRotation()
+    {
+        _shouldRotate = false;
+    }
+}
