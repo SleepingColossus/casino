123516b0b46dbbd3d42de92c731eca9a52566ba4
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-03-27 22:27:32 +0000

    Vary spin speeds and stop

---
+++ Assets/Scripts/ReelSpeed.cs
@@ -0,0 +1,7 @@
+public enum ReelSpeed
+{
+    Fast,
+    Medium,
+    Slow,
+    Idle
+}
--- Assets/Scripts/SpinReel.cs
+++ Assets/Scripts/SpinReel.cs
@@ -5,32 +5,77 @@ using UnityEngine.Serialization;
 
 public class SpinReel : MonoBehaviour
 {
-    public float rotationSpeed = 1.0f;
+    public float rotationSpeedFast = 1.0f;
+    public float rotationSpeedMedium = 1.0f;
+    public float rotationSpeedSlow = 1.0f;
+    public float slowdownPeriod = 2.0f;
 
-    private bool _shouldRotate;
+    private float _slowdownElapsed;
+    private ReelSpeed _reelSpeed;
     
     // Start is called before the first frame update
     void Start()
     {
-        _shouldRotate = false;
+        _slowdownElapsed = 0.0f;
+        _reelSpeed = ReelSpeed.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_shouldRotate)
+        if (ShouldRotate())
         {
+            float rotationSpeed;
+
+            if (_reelSpeed == ReelSpeed.Fast)
+            {
+                rotationSpeed = rotationSpeedFast;
+            }
+            else if (_reelSpeed == ReelSpeed.Medium)
+            {
+                rotationSpeed = rotationSpeedMedium;
+            }
+            else
+            {
+                rotationSpeed = rotationSpeedSlow;
+            }
+
             transform.Rotate(0, 0, -rotationSpeed);
+            
+            _slowdownElapsed += Time.deltaTime;
+
+            Debug.Log(_slowdownElapsed);
+            
+            if (_slowdownElapsed >= slowdownPeriod)
+            {
+                _reelSpeed = SlowDown(_reelSpeed);
+                _slowdownElapsed = 0;
+            }
         }
     }
 
     public void StartRotation()
     {
-        _shouldRotate = true;
+        _reelSpeed = ReelSpeed.Fast;
     }
 
-    public void StopRotation()
+    private bool ShouldRotate()
     {
-        _shouldRotate = false;
+        return _reelSpeed != ReelSpeed.Idle;
+    }
+
+    private ReelSpeed SlowDown(ReelSpeed rs)
+    {
+        if (rs == ReelSpeed.Fast)
+        {
+            return ReelSpeed.Medium;
+        }
+
+        if (rs == ReelSpeed.Medium)
+        {
+            return ReelSpeed.Slow;
+        }
+
+        return ReelSpeed.Idle;
     }
 }
