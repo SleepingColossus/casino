7eaf2b3ee444987e975123ccf4c8309a74d6fd86
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-04-18 20:56:23 +0100

    Add mute buttons

--- Assets/Scripts/ButtonStyler.cs
+++ Assets/Scripts/ButtonStyler.cs
@@ -9,9 +9,16 @@ public class ButtonStyler : MonoBehaviour
     public Button wager100Button;
     public Button wager1000Button;
 
+    public Button muteMusicButton;
+    public Button muteSoundButton;
+
+    private readonly Color _activeColor = Color.yellow;
+    private readonly Color _inactiveColor = Color.white;
+    
+    
     void Awake()
     {
-        wager100Button.image.color = Color.yellow;
+        wager100Button.image.color = _activeColor;
     }
 
     public void AdjustColor(Button b)
@@ -25,9 +32,14 @@ public class ButtonStyler : MonoBehaviour
 
         foreach (var button in allButtons)
         {
-            button.image.color = Color.white;
+            button.image.color = _inactiveColor;
         }
 
-        b.image.color = Color.yellow;
+        b.image.color = _activeColor;
+    }
+
+    public void ToggleColor(Button b)
+    {
+        b.image.color = b.image.color == _inactiveColor ? _activeColor : _inactiveColor;
     }
 }
---
+++ Assets/Scripts/MuteController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteController : MonoBehaviour
+{
+    public bool muteMusic;
+    public bool muteSound;
+
+    private AudioSource _musicSource;
+    public AudioSource[] _soundSources;
+
+    public void Start()
+    {
+        _musicSource = GetComponent<AudioSource>();
+    }
+    
+
+    public void ToggleMusic()
+    {
+        muteMusic = !muteMusic;
+        _musicSource.mute = muteMusic;
+    }
+
+    public void ToggleSound()
+    {
+        muteSound = !muteSound;
+
+        foreach (var soundSource in _soundSources)
+        {
+            soundSource.mute = muteSound;
+        }
+    }
+}
