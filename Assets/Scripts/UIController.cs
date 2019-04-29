a8b2fd6b7ac781d6e0366b0debcdbda66e539179
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-04-29 21:16:11 +0100

    Add sprites to indicate audio status

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,6 +7,16 @@ public class UIController : MonoBehaviour
     public Button wager100Button;
     public Button wager1000Button;
 
+    public Button toggleMusicButton;
+    public Image toggleMusicIcon;
+    public Button toggleSoundButton;
+    public Image toggleSoundIcon;
+
+    public Sprite musicOnSprite;
+    public Sprite musicOffSprite;
+    public Sprite soundOnSprite;
+    public Sprite soundOffSprite;
+
     private readonly Color _activeColor = Color.yellow;
     private readonly Color _inactiveColor = Color.white;
 
@@ -16,6 +26,20 @@ public class UIController : MonoBehaviour
     public void ToggleColor(Button b)
     {
         b.image.color = b.image.color == _inactiveColor ? _activeColor : _inactiveColor;
+        ToggleIcon(b, b.image.color == _activeColor);
+    }
+
+    private void ToggleIcon(Button b, bool isOn)
+    {
+        if (b == toggleMusicButton)
+        {
+            toggleMusicIcon.sprite = isOn ? musicOffSprite : musicOnSprite;
+        }
+
+        if (b == toggleSoundButton)
+        {
+            toggleSoundIcon.sprite = isOn ? soundOffSprite : soundOnSprite;
+        }
     }
 
     public void UpdateUIState(int balance)
