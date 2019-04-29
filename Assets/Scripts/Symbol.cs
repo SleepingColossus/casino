6796f075a029c333b775d32dbdfe21eae67f928b
Aleksandar Mancic <AcaMancic@users.noreply.github.com>
2019-04-29 15:06:11 +0100

    Change coin drop amount

--- Assets/Scripts/Symbol.cs
+++ Assets/Scripts/Symbol.cs
@@ -6,12 +6,12 @@ public static class Symbol
     public static readonly Dictionary<SymbolType, SymbolMetadata> SymbolMetadata =
         new Dictionary<SymbolType, SymbolMetadata>()
         {
-            {SymbolType.Bar1, new SymbolMetadata(0, new Reward(4, 10))},
-            {SymbolType.Bar2, new SymbolMetadata(60, new Reward(8, 10))},
-            {SymbolType.Bell, new SymbolMetadata(120, new Reward(3, 10))},
+            {SymbolType.Bar1, new SymbolMetadata(0, new Reward(4, 15))},
+            {SymbolType.Bar2, new SymbolMetadata(60, new Reward(8, 18))},
+            {SymbolType.Bell, new SymbolMetadata(120, new Reward(3, 13))},
             {SymbolType.Cherry, new SymbolMetadata(180, new Reward(2, 10))},
             {SymbolType.Grape, new SymbolMetadata(240, new Reward(2, 10))},
-            {SymbolType.Dollar, new SymbolMetadata(300, new Reward(10, 10))}
+            {SymbolType.Dollar, new SymbolMetadata(300, new Reward(10, 20))}
         };
 
     public static SymbolType PickRandomSymbol()
